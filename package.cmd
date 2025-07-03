b36e767fe774fe1f71e07766c0ceee27ad6f4976
zhouzhoushen <zhouzhoushen@seu.edu.cn>
2025-07-03 23:40:56 +0800

    feat: add scripts for boosting and packaging

---
+++ package.cmd
@@ -0,0 +1,2 @@
+poetry run pyinstaller  --onefile --clean --log-level=INFO --icon=favicon.ico --name anti_procrastination cli.py
+:: --noconsole --add-data "cli.py;." --hidden-import=rich --collect-all=tkinter 
--- run.cmd
+++ run.cmd
@@ -1,1 +1,13 @@
+@echo off
+cd /d "%~dp0"
+
+:: Activate virtual environment
+call conda activate py3.12
+
+:: Directly call poetry (now in the activated environment)
 poetry run python cli.py
+
+if %errorlevel% neq 0 (
+    echo [Error] CLI execution failed.
+)
+pause
